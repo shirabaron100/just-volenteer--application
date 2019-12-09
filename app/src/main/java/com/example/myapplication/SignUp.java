1b0937bd7f517c69e74c0fc9b7cdb4eac9e1a674
DELL <hadar64@gmail.com>
2019-12-09 12:59:13 +0200

    connect to the firebase

--- app/src/main/java/com/example/myapplication/SignUp.java
+++ app/src/main/java/com/example/myapplication/SignUp.java
@@ -1,25 +1,149 @@
 package com.example.myapplication;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 import android.widget.Button;
 
+import com.example.myapplication.models.User;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class SignUp extends AppCompatActivity {
 
    private TextView in_to_up;
-    EditText name, email, password, phone, T_shirt_size;
+   EditText mFullName, mEmail, mPassword, mPhone, T_shirt_size;
+   Button mRegisssterBtn;
+   TextView mLginBtn;
+   FirebaseAuth fAuth;
+   ProgressBar progressBar;
+   Spinner spinner;
+   ArrayAdapter <CharSequence> adapter;
+
+   // DB
+   FirebaseDatabase database;
+   DatabaseReference myRef;
+
+   @Override
+    protected void onCreate(Bundle savedIdInstanceState){
+       super.onCreate(savedIdInstanceState);
+       setContentView(R.layout.sign_up);
+
+           // Init db
+       database = FirebaseDatabase.getInstance();
+
+       mFullName =findViewById(R.id.name);
+       mEmail= findViewById(R.id.email_IN);
+       mPassword= findViewById(R.id.password_IN);
+       //mPhone= findViewById(R.id.);
+       mRegisssterBtn= findViewById(R.id.register_now);
+       mLginBtn= findViewById(R.id.already_login);
+       fAuth = FirebaseAuth.getInstance();
+       progressBar = findViewById(R.id.progressBar2);
+
+       if(fAuth.getCurrentUser() != null ){
+           startActivity(new Intent(getApplicationContext(),SignIn.class));
+           finish();
+       }
+
+       mRegisssterBtn.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View view) {
+                String email= mEmail.getText().toString().trim();
+                String password = mPassword.getText().toString().trim();
+
+                if(TextUtils.isEmpty(email)){
+                    mEmail.setError("Email is Requierd.");
+                    return;
+                }
+
+                if(TextUtils.isEmpty(password)){
+                    mPassword.setError("password is Requierd.");
+                    return;
+                }
+                if(password.length()<6){
+                    mPassword.setError("Password Must be>= 6 Characters");
+                    return;
+                }
+
+                progressBar.setVisibility(view.VISIBLE);
+
+                fAuth.createUserWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()){
+                            Toast.makeText(SignUp.this, "User Created", Toast.LENGTH_SHORT).show();
+                            startActivity(new Intent(getApplicationContext(),SignIn.class));
+
+                            User user = new User(mEmail.getText().toString(), mFullName.getText().toString());
+
+                            String uid = task.getResult().getUser().getUid();
+
+                            myRef = database.getReference("users/" + uid);
+
+                            myRef.setValue(user);
+
+                        }else{
+                            Toast.makeText(SignUp.this, "Error ! "+ task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+
+           }
+
+                                         }
+
+       );
+   }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+   /*EditText name, email, password, phone, T_shirt_size;
     Spinner spinner;
     ArrayAdapter <CharSequence> adapter;
+    FirebaseAuth fAuth;
+    Button mRegisterBtn;
+    ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,6 +157,31 @@ public class SignUp extends AppCompatActivity {
         adapter = ArrayAdapter.createFromResource(this, R.array.Tshirt_size, android.R.layout.simple_spinner_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner.setAdapter((adapter));
+        fAuth=FirebaseAuth.getInstance();
+        mRegisterBtn= findViewById(R.id.register_now);
+        //progressBar = findViewById(R.id.progresBar);
+        mRegisterBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+             String Email=email.getText().toString().toString().trim();
+             String Password=password.getText().toString().trim();
+
+             if(TextUtils.isEmpty(Email)){
+                 email.setError("Emaili is Required.");
+                 return;
+             }
+             if(TextUtils.isEmpty(Password)){
+                 password.setError("Emaili is Required.");
+                 return;
+             }
+             if(password.length()<6){
+               password.setError("password Must be >= 6 characters");
+               return;
+             }
+                progressBar.setVisibility(view.VISIBLE);
+            }
+            fAuth.createUserWithEmailAndPassword(Email,password).addOncompleteL
+        });
         spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
@@ -56,7 +205,7 @@ public class SignUp extends AppCompatActivity {
     private void openSignIN() {
             Intent intent=new Intent(this,SignIn.class);
             startActivity(intent);
-        }
+        }*/
     }
 
 
--- app/src/main/java/com/example/myapplication/models/User.java
+++ app/src/main/java/com/example/myapplication/models/User.java
@@ -1,6 +1,9 @@
 package com.example.myapplication.models;
 
 public class User {
+    public User(String name, String email) {
+
+    }
     // name, email, tsize, phone
     private String name, email;
     private int phone;
